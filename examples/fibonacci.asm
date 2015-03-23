cef8655d7513fb161a2f081109a7b21b4b6d6653
Kevin Vu <kevin.m.vu@gmail.com>
2015-03-23 00:45:10 -0700

    refactor, document fib example

--- examples/fibonacci.asm
+++ examples/fibonacci.asm
@@ -1,36 +1,50 @@
-jmp .main
-
-.fib
-push %fp
-mov %sp %fp
-push %d
-mov 2(%fp) %d
-cmp %d $0      # check if n = 0
-je .fib_0
-cmp %d $1      # check if n = 1
-je .fib_1
-jmp .fib_else  # handle n >= 2
-.fib_0
-mov $0 %a
-jmp .fib_ret
-.fib_1
-mov $1 %a
-jmp .fib_ret
-.fib_else
-push %d
-dec (%sp)
-call .fib      # first recursive call
-mov %a %d
-dec (%sp)
-call .fib      # second recursive call
-add %d %a
-.fib_ret
-mov -1(%fp) %d
-leave
-ret
-
-# push argument and call .fib
+## MAIN METHOD
 .main
-push $7
-call .fib
-p
+  # push argument and call .fib
+  push $7
+  call .fib
+  p
+  exit
+
+## RECURSIVE FIBONACCI SUBROUTINE
+.fib
+  # standard x86 calling procedure
+  push %fp       # save old frame pointer
+  mov %sp %fp    # set current frame pointer
+  
+  # read arguments
+  push %d        # save 'callee-saved' register
+  mov 2(%fp) %d  # move argument to %d
+  
+  # if else jump structure
+  cmp %d $0      # if n = 0
+  je .fib_0
+  cmp %d $1      # else if n = 1
+  je .fib_1
+  jmp .fib_else  # else (if n >= 2)
+
+# if n = 0
+.fib_0
+  mov $0 %a      # set return value
+  jmp .fib_ret
+
+# if n = 1
+.fib_1
+  mov $1 %a      # set return value
+  jmp .fib_ret
+
+# if n >= 2
+.fib_else
+  push %d
+  dec (%sp)
+  call .fib      # first recursive call
+  mov %a %d
+  dec (%sp)
+  call .fib      # second recursive call
+  add %d %a
+
+.fib_ret
+  # standard x86 return procedure
+  mov -1(%fp) %d # restore 'callee-saved' register
+  leave          # restore stack and frame pointers
+  ret            # return to caller's next instruction
